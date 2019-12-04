4022cbc5f6f017c6ec91f61b37e2a88f60d14994
rizwanishaq <rizwanishaq@gmail.com>
2019-12-04 16:49:34 +0100

    Updates

---
+++ precache-manifest.68467a0ac464ab7964e88e84fb1ed298.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1df7e81b2fd54ef83f8c67d6c1490c10",
+    "url": "/simpletodoTiffInTech/index.html"
+  },
+  {
+    "revision": "4e38f80c0cda3214efb9",
+    "url": "/simpletodoTiffInTech/static/css/main.b100e6da.chunk.css"
+  },
+  {
+    "revision": "06166b154159c60b70c4",
+    "url": "/simpletodoTiffInTech/static/js/2.a4b6d6c6.chunk.js"
+  },
+  {
+    "revision": "4e38f80c0cda3214efb9",
+    "url": "/simpletodoTiffInTech/static/js/main.448f6f54.chunk.js"
+  },
+  {
+    "revision": "02e4cbe89e82e269cca4",
+    "url": "/simpletodoTiffInTech/static/js/runtime-main.a8f27b04.js"
+  }
+]);
